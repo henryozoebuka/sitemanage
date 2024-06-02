71276f59ebd5dba000fa4199ea1f215414e835de
unknown <hintlord@gmail.com>
2024-06-02 13:24:22 +0100

    sitemanage mobile app first commit.

---
+++ sitemanage/constants/styles.js
@@ -0,0 +1,117 @@
+export const styles = {
+
+    // Padding dimensions
+    padding5: {
+        padding: 5
+    },
+
+    padding10: {
+        padding: 10
+    },
+
+    padding15: {
+        padding: 15
+    },
+
+    padding20: {
+        padding: 20
+    },
+
+    padding25: {
+        padding: 25
+    },
+
+    padding30: {
+        padding: 30
+    },
+
+    padding35: {
+        padding: 35
+    },
+
+    padding40: {
+        padding: 40
+    },
+
+    padding45: {
+        padding: 45
+    },
+
+    padding50: {
+        padding: 50
+    },
+
+    // Margin dimensions
+
+    margin5: {
+        margin: 5
+    },
+
+    margin10: {
+        margin: 10
+    },
+
+    margin15: {
+        margin: 15
+    },
+
+    margin20: {
+        margin: 20
+    },
+
+    margin25: {
+        margin: 25
+    },
+
+    margin30: {
+        margin: 30
+    },
+
+    margin35: {
+        margin: 35
+    },
+
+    margin40: {
+        margin: 40
+    },
+
+    margin45: {
+        margin: 45
+    },
+
+    margin50: {
+        margin: 50
+    },
+
+    button: {
+        backgroundColor: 'blue',
+        padding: 10,
+        flex: 1,
+        justifyContents: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        borderRadius: 25,
+    },
+
+    textInput: {
+        backgroundColor: '#ffffff',
+        padding: 10,
+        flex: 1,
+        justifyContents: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        borderRadius: 25,
+        borderWidth: 1,
+        borderColor: 'gray'
+    },
+
+    safeAreaView : {
+        padding: 20
+    },
+
+    view: {
+        padding: 20
+    }
+}
+
+
---
+++ sitemanage/server/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import 'dotenv/config'
+
+const start =  async () => {
+    const app = express();
+    app.use(express.json());
+    app.use(cors())
+    
+    const port = process.env.PORT
+
+
+    if (!process.env.MONGO_URI) {
+        throw new error('auth MONGO_URI must be defined');
+
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+            .then(console.log('Connected to database'))
+            .catch(e => { console.log(e.message) })
+
+    } catch (error) {
+        console.log(error)
+
+    }
+
+    app.listen(port, () => {
+        console.log(`Server connected on port ${port}`)
+    })
+}
+
+start()
+
+
